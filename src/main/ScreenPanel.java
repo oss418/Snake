686db2fd4116aa8aa5adf09db4312ab6f04bfb24
oss418 <133474578+oss418@users.noreply.github.com>
2023-05-20 09:01:59 -0700

    Movement

--- src/main/ScreenPanel.java
+++ src/main/ScreenPanel.java
@@ -7,11 +7,16 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class ScreenPanel extends JPanel implements ActionListener{
-
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	private void loadIcon() {
+		ImageIcon myImageYAY = new ImageIcon("src/resources/IMAGENAME.png");
+        Image snake_pic = myImageYAY.getImage();
 	}
 
 }
--- src/main/snake.java
+++ src/main/snake.java
@@ -1,53 +1,39 @@
 package main;
 
-public class snake {
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
+public class snake extends KeyAdapter {
+	boolean up = false;
+	boolean down = false;
+	boolean right = false;
+	boolean left = false;
+	@Override
+	public void keyPressed(KeyEvent event) {
+		int key = event.getKeyCode();
+		if ((key == KeyEvent.VK_UP) && !down) {
+			up = true;
+			right = false;
+			left = false;
+		}
+		if ((key == KeyEvent.VK_DOWN) && !up) {
+			down = true;
+			right = false;
+			left = false;
+		}
+		if ((key == KeyEvent.VK_RIGHT) && !left) {
+			right = true;
+			down = false;
+			up = false;
+		}
+		if ((key == KeyEvent.VK_LEFT) && !right) {
+			left = true;
+			down = false;
+			up = false;
+		}
+	}
+
+	public void move() {
+		
+	}
 }
-//movement
-//public void moveUp(map screen, snake mySnake) {
-//	if (mySnake.getX() <= 0) {
-//		mySnake.setX(screen.getWidth() - 1);
-//		screen.setObjectOnSpot(mySnake, mySnake.getX(), mySnake.getY());
-//		screen.reset(0, mySnake.getY());
-//	} else {
-//		mySnake.setX(getX() - 1); // moving snake
-//		screen.setObjectOnSpot(mySnake, mySnake.getX(), mySnake.getY()); // putting the snake part on that box
-//		screen.reset(mySnake.getX() + 1, mySnake.getY()); // clear the box before the snake move
-//	}
-//}
-//
-//public void moveDown(map screen, snake mySnake) {
-//	if (mySnake.getX() >= screen.getWidth() - 1) {
-//		mySnake.setX(0);
-//		screen.setObjectOnSpot(mySnake, mySnake.getX(), mySnake.getY());
-//		screen.reset(screen.getHeight() - 1, mySnake.getY());
-//	} else {
-//		mySnake.setX(getX() + 1);
-//		screen.setObjectOnSpot(mySnake, mySnake.getX(), mySnake.getY());
-//		screen.reset(mySnake.getX() - 1, mySnake.getY());
-//	}
-//}
-//
-//public void moveLeft(map screen, snake mySnake) {
-//	if (mySnake.getY() <= 0) {
-//		mySnake.setY(screen.getHeight() - 1);
-//		screen.setObjectOnSpot(mySnake, mySnake.getX(), mySnake.getY());
-//		screen.reset(mySnake.getX(), 0);
-//	} else {
-//		mySnake.setY(getY() - 1);
-//		screen.setObjectOnSpot(mySnake, mySnake.getX(), mySnake.getY());
-//		screen.reset(mySnake.getX(), mySnake.getY() + 1);
-//	}
-//}
-//
-//public void moveRight(map screen, snake mySnake) {
-//	if (mySnake.getY() >= screen.getHeight() - 1) {
-//		mySnake.setY(0);
-//		screen.setObjectOnSpot(mySnake, mySnake.getX(), mySnake.getY());
-//		screen.reset(mySnake.getX(), screen.getHeight() - 1);
-//	} else {
-//		mySnake.setY(getY() + 1);
-//		screen.setObjectOnSpot(mySnake, mySnake.getX(), mySnake.getY());
-//		screen.reset(mySnake.getX(), mySnake.getY() - 1);
-//	}
-//}
